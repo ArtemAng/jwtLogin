c53d87c39371cb1bcf7f6f6aeab5279248972a7b
ArtemAng <eviljack89@gmail.com>
2020-12-19 00:25:07 +0300

    deployment

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes.js'))
 app.use('/api/usersList', require('./routes/users.routes'))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./client/build'))
+}
+
 const start = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
@@ -23,8 +27,5 @@ const start = async () => {
         console.log('server error', e.message);
         process.exit(1);
     }
-}
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'))
 }
 start();
