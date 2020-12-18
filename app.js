fee433753683bc8c135fe3c5b1617bd0848780a6
ArtemAng <eviljack89@gmail.com>
2020-12-19 00:45:35 +0300

    deployment

--- app.js
+++ app.js
@@ -9,23 +9,25 @@ app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes.js'))
 app.use('/api/usersList', require('./routes/users.routes'))
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://aaa:aaa123@cluster0.nhuv4.mongodb.net/users?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 }
+// const start = async () => {
+//     try {
 
-const start = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-        app.listen(PORT, () => console.log('App has been started'));
-
-    }
-    catch (e) {
-        console.log('server error', e.message);
-        process.exit(1);
-    }
-}
-start();
+        
+//     }
+//     catch (e) {
+//         console.log('server error', e.message);
+//         process.exit(1);
+//     }
+// }
+app.listen(PORT, () => console.log('App has been started'));
+// start();
