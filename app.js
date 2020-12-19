8a5371caac80d51ba5d2fc3724e2643de4bb3c46
ArtemAng <eviljack89@gmail.com>
2020-12-19 15:45:42 +0300

    sad

--- app.js
+++ app.js
@@ -1,40 +1,3 @@
-// const express = require('express');
-// const config = require('config');
-// const mongoose = require('mongoose')
-
-// const app = express();
-// const PORT = process.env.PORT || 8888;
-
-// app.use(express.json({ extended: true }))
-// app.use('/api/auth', require('./routes/auth.routes.js'))
-// app.use('/api/usersList', require('./routes/users.routes'))
-
-
-// const start = async () => {
-//     try {
-        
-//         if (process.env.NODE_ENV === 'production') {
-//             app.use(express.static('client/build'))
-//         }
-//         await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://aaa:aaa123@cluster0.nhuv4.mongodb.net/users?retryWrites=true&w=majority', {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             useCreateIndex: true
-//         });
-//         app.listen(PORT, () => console.log('App has been started'));
-
-        
-//     }
-//     catch (e) {
-//         console.log('server error', e.message);
-//         process.exit(1);
-//     }
-// }
-// start();
-
-
-
-
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose')
@@ -46,7 +9,7 @@ app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes.js'))
 app.use('/api/usersList', require('./routes/users.routes'))
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://f:f@cluster0.kutye.mongodb.net/f?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://Artem:aaa123@cluster0.qj5z6.mongodb.net/app?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -59,14 +22,4 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 app.listen(PORT, () => console.log('App has been started'));
-// const start = async () => {
-//     try {
 
-        
-//     }
-//     catch (e) {
-//         console.log('server error', e.message);
-//         process.exit(1);
-//     }
-// }
-// start();
